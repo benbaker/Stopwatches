0dbff03155ef460dddecfa8fbe5c04d5c64ad381
Joseph <josephFolkemer@gmail.com>
2013-12-20 20:44:35 -0600

    first gh push (testing)

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http	= require("http"),
 	jade	= require("jade"),
 	redis	= require("redis"),
 	moment	= require("moment"),
+	util	= require("util"),
 	app		= express();
 
 var server = {
@@ -25,8 +26,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use(function(req, res, next){
-  console.log("%s - %s '%s'", now(), req.method, req.url);
-  next();
+	util.log(req.method +" : '" + req.url + "'");
+	next();
 });
 
 
