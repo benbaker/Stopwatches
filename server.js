cb31844c0b9d8df85af4c630b163b91600cb820e
Joseph <josephFolkemer@gmail.com>
2013-12-20 19:59:11 -0600

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+var http	= require("http"),
+	url		= require("url"),
+	path	= require('path'),
+	express= require("express"),
+	jade	= require("jade"),
+	redis	= require("redis"),
+	moment	= require("moment"),
+	app		= express();
+
+var server = {
+	port:8888
+};
+
+jade.options = {
+	pretty: true
+};
+
+var now = function(){
+	return moment().format('H:mm:ss');
+};
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+app.use(function(req, res, next){
+  console.log("%s - %s '%s'", now(), req.method, req.url);
+  next();
+});
+
+
+app.get('/', function(req, res){
+	res.render('stopwatches', jade.options);
+	res.end('Hi there!');
+});
+
+
+
+app.listen(server.port);
+console.log('server port: ' + server.port);
